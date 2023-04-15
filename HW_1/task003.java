998a018cccb606b3fef9c943201461ffe27e0e76
ilyanik77 <ilyakhlamov77@gmail.com>
2023-04-15 17:02:53 +0300

    Add task003

--- HW_1/task003.java
+++ HW_1/task003.java
@@ -3,7 +3,8 @@ import java.util.ArrayList;
 public class task003 {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] arr2 = {0, 1, 2, 3, 3, 2, 1, 2, };
+        int[] arr2 = {0, 1, 2, 3, 3, 2, 1, 2, 3};
+        //int[] arr2 = {0, 1, 2, 3, 3, 2, 1, 2};
         //int[] arr2 = {};
         System.out.println(differenceArray(arr1,arr2));
     }
